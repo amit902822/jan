0fe6c017d95d0b4b538d8038603f163ea1354171
Amit <amit.behra1995@gmail.com>
2023-09-08 23:19:38 +0530

    first commit

---
+++ projects/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project
+
+ProjectsList = [
+    {
+        'id':'1',
+        'title':"Ecommerce Website",
+        'description':'Fully functional ecommerce website'
+    },
+    {
+        'id':'2',
+        'title':"Portfolia Website",
+        'description':'This was a project where I built out my portfolia'
+
+    },
+    {
+        'id':'3',
+        'title':"Social Network",
+        'description':'Awesome open source project I am still working'
+    },
+]
+
+
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects':projects}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    return render(request, 'projects/single-project.html', {'project':projectObj})
+
+
